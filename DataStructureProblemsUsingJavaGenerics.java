9f5b682e78c7babb2775c3b277de339c480d18aa
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 17:41:13 +0530

    Refractor: Code for creating a linked list.

---
+++ DataStructureProblemsUsingJavaGenerics.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class DataStructureProblemsUsingJavaGenerics {
+	
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+	
+		linkedList.add(56);
+		linkedList.add(30);
+		linkedList.add(70);
+		
+		System.out.println("Elements in the list are =>" + linkedList);
+	}
+}
