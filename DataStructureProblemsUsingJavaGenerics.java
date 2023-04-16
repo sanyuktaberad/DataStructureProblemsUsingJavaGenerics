24d24fd34e76a6933db9cd1c0d67791bc44b83bf
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 22:12:26 +0530

    Add: code for adding new elements is added.

--- DataStructureProblemsUsingJavaGenerics.java
+++ DataStructureProblemsUsingJavaGenerics.java
@@ -7,10 +7,13 @@ public class DataStructureProblemsUsingJavaGenerics {
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
 	
-		linkedList.add(56);
+		linkedList.addLast(70);
 		linkedList.add(30);
-		linkedList.add(70);
+		linkedList.addFirst(56);
 		
 		System.out.println("Elements in the list are =>" + linkedList);
+		
+	
+		
 	}
 }
