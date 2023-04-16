cc7b0b10f08b1a8a15ac62e85df9e5107ea0aac3
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 23:02:32 +0530

    Add: code for adding an element in between linkedlist is added.

--- DataStructureProblemsUsingJavaGenerics.java
+++ DataStructureProblemsUsingJavaGenerics.java
@@ -3,17 +3,15 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 public class DataStructureProblemsUsingJavaGenerics {
-	
+
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
-	
-		linkedList.addLast(70);
-		linkedList.add(30);
-		linkedList.addFirst(56);
-		
+
+		linkedList.add(56);
+		linkedList.add(70);
+		linkedList.set(2, 30);
+
 		System.out.println("Elements in the list are =>" + linkedList);
-		
 	
-		
 	}
 }
